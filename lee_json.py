5900f9115568c784fdbb834cb77fcd8ccadf1d6f
AngelaOrtiz <angelaortizvarela@gmail.com>
2022-10-11 22:11:57 -0300

    continuar

--- lee_json.py
+++ lee_json.py
@@ -1,8 +1,8 @@
 import json
-mi_json = open("datos.json","r",encoding="utf-8")
+mi_json = open("datos.json","r", encoding="utf-8")
 print(mi_json)
 json_datos = mi_json.read()
-datos = json.loads("json_datos")
-print("Json_datos: ",json_datos)
-print("Datos: ",datos)
-print("Temperaturas: ",datos["temperaturas"])
+datos = json.loads(json_datos)
+print("json_datos: ", json_datos)
+print("datos: ", datos)
+print("temperaturas: ", datos["temperaturas"])
